2388c3bd7ed1ca824a35c312c7f9d8d8d00a12d9
Pinguin130 <vamosteamd@gmail.com>
2022-04-24 00:53:03 -0300

    Initial commit

---
+++ src/main/java/dev/pinguin/afk/Afktime.java
@@ -0,0 +1,18 @@
+package dev.pinguin.afk;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Afktime extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
+
